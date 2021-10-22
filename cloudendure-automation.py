10c8d3ea46c9d002c648045b08b2b872183bf32d
Boris Yakimov <yakimovb@gmail.com>
2021-10-22 17:07:02 +0300

    connect logic for taking values from source ec2 and update in destination blueprint

--- cloudendure-automation.py
+++ cloudendure-automation.py
@@ -3,9 +3,9 @@ import requests, boto3, json, sys
 from botocore.exceptions import ClientError
 from argparse import ArgumentParser
 
+# TODO: better erorr handling on HTTP requests - use raise_for_status()
+# TODO: implement debug logging option
 def main():
-    # TODO: Build a list of Security Groups and Subnets from eu-west-1 and make sure the same ones are set in blueprint
-    # TODO: better erorr handling on HTTP requests - use raise_for_status()
     # Get AWS account name from input param
     parser = ArgumentParser()
     parser.add_argument("--accountName", help="Provide AWS Account Name(ex. ecint-non-prod)", required=True)
@@ -18,62 +18,80 @@ def main():
     # Init EC2 Client
     ec2_client = boto3.client('ec2')
 
-    # Main API URL
+    # Main Cloudendure API URL
     cloudendure_url = "https://console.cloudendure.com/api/latest"
+    print(f'Cloudendure API URL set to {cloudendure_url}')
 
     # Authenticate in Cloudendure
     api_key = "6F1A-C693-6F14-0E7C-F296-C4BE-5CF5-269A-017E-D864-B9D1-2BD6-5693-6A0F-622D-E7E2"
     authenticate(http_client, cloudendure_url, api_key)
 
-    # Get list of project names and their IDs
+    # Get project ID
     project_json_configs = list_projects(http_client, cloudendure_url)
     for project in project_json_configs['items']:
         project_name = project['name']
         project_id = project['id']
-        # projects[project_name] = project_id
         if project_name == input_args.accountName:
+            print(f'Working with project [{project_name}]')
             cloudendure_project_id = project_id
 
-    # TODO: find blueprint id from machine ?
-    cloudendure_blueprint_id = "f320947e-1555-4cee-9128-58a6cc4dd99c"
+    # Get blueprint objects
+    blueprint_json_configs = list_blueprints(http_client, cloudendure_url, cloudendure_project_id)
 
-    # Get list of machine objects in each project
-    machine_json_configs = list_machines(http_client, cloudendure_url, project_id)
+    # Build blueprint id to machine id mapping
+    blueprint_id_map = {}
+    for blueprint in blueprint_json_configs['items']:
+            cloudendure_machine_id = blueprint['machineId']
+            cloudendure_blueprint_id = blueprint['id']
+            blueprint_id_map[cloudendure_machine_id] = cloudendure_blueprint_id
+
+    # Get machine objects 
+    machine_json_configs = list_machines(http_client, cloudendure_url, cloudendure_project_id)
+
+    # Update blueprint in each cloudendure machine
     for machine in machine_json_configs['items']:
+        # Get source EC2's instance id and name
         source_ec2_name = machine['sourceProperties']['name']
         source_ec2_id = machine['sourceProperties']['machineCloudId']
+
+        # Get security groups from source EC2
+        print(f'Get Security Groups from instance {source_ec2_name} / {source_ec2_id}')
+        sg_map = get_ec2_instance_sgs(ec2_client, source_ec2_id)
+        change_config = "securityGroupIDs"
+        security_groups = {}
+        for security_group in sg_map:
+            sg_name = security_group['GroupName']
+            sg_id = security_group['GroupId']
+            security_groups[sg_name] = sg_id
+        print(f'Security groups to be applied {security_groups}')
+
+        # Update security groups in target machine blueprint to match source's
         cloudendure_machine_id = machine['id']
-        # print("source ec2 name:", source_ec2_name)
-        # print("source ec2 id:", source_ec2_id)
-        # print("cloudendure machine id:", cloudendure_machine_id)
+        print(f'Updating Cloudendure Machine with ID: {cloudendure_machine_id}')
+        for machine_id, blueprint_id in blueprint_id_map.items():
+            if cloudendure_machine_id == machine_id:
+                # SG
+                print(f'Updating Security Groups in Blueprint with ID: {cloudendure_machine_id}')
+                # TODO: temp hardcoded values until tested in internal network
+                security_groups = {
+                    'private_db_ecint': 'sg-0244a14e569eaba68',
+                    'private_active_directory_client': 'sg-3247085f',
+                    'private_db': 'sg-c54906a8',
+                    'console': 'sg-d64807bb'
+                    }
+                update_blueprint(http_client, cloudendure_url, cloudendure_project_id, blueprint_id, machine_id, change_config, security_groups)
 
-    # Get blueprint config json
-    blueprint_config = get_blueprint(http_client, cloudendure_url, cloudendure_project_id, cloudendure_blueprint_id)
+                # TODO: test and fix subnets
+                # Subnets
+                # TODO: temp hardcoded values until tested in internal network
+                # subnet = {
+                    # "test_subnet": "subnet-00741c4d"
+                # }
+                # print(f'Updating Subnet in Blueprint with ID: {cloudendure_machine_id}')
+                # update_blueprint(http_client, cloudendure_url, cloudendure_project_id, blueprint_id, machine_id, change_config, subnet)
 
-    # Get machine id from blueprint config
-    machine_id = blueprint_config['machineId']
-
-    # Get security groups from source EC2 
-    sg_map = get_ec2_instance_sgs(ec2_client, source_ec2_id)
-    change_config = "securityGroupIDs"
-    security_groups = {}
-    for security_group in sg_map:
-        sg_name = security_group['GroupName']
-        sg_id = security_group['GroupId']
-        security_groups[sg_name] = sg_id
-
-    security_groups = {
-        'private_db_ecint': 'sg-0244a14e569eaba68',
-        'private_active_directory_client': 'sg-3247085f',
-        'private_db': 'sg-c54906a8',
-        'console': 'sg-d64807bb'
-        }
-
-    # Update blueprint's security groups 
-    update_blueprint(http_client, cloudendure_url, cloudendure_project_id, cloudendure_blueprint_id, machine_id, change_config, security_groups)
-
-    # # TODO: Update blueprint's subnet
-    # update_blueprint(http_client, cloudendure_url, cloudendure_project_id, cloudendure_blueprint_id, machine_id, change_config, security_groups)
+                # TODO: print blueprint json after update in case of debug flag 
+                #print(get_blueprint(http_client, cloudendure_url, cloudendure_project_id, blueprint_id))
 
 def authenticate(http_client, cloudendure_url, api_key):
     login_url = cloudendure_url + "/login"
@@ -101,6 +119,7 @@ def list_projects(http_client, cloudendure_url):
     return project_list
 
 def list_machines(http_client, cloudendure_url, cloudendure_project_id):
+    # Get Machines definition and return as JSON
     machines_url = cloudendure_url + "/projects/" + cloudendure_project_id + "/machines"
     resp = http_client.get(url = machines_url)
 
@@ -110,6 +129,18 @@ def list_machines(http_client, cloudendure_url, cloudendure_project_id):
         raise Exception('Unable to get list of machines, response code:', resp.status_code, resp.reason)
 
     return machine_list
+
+def list_blueprints(http_client, cloudendure_url, cloudendure_project_id):
+    # Get Projects definition and return as JSON
+    blueprints_url = cloudendure_url + "/projects/" + cloudendure_project_id + "/blueprints"
+    resp = http_client.get(url = blueprints_url)
+
+    blueprint_list = resp.json()
+
+    if resp.status_code != 200:
+        raise Exception('Unable to get list of projects, response code:', resp.status_code, resp.reason)
+
+    return blueprint_list
 
 def get_blueprint(http_client, cloudendure_url, cloudendure_project_id, cloudendure_blueprint_id):
     blueprint_url = cloudendure_url + "/projects/" + cloudendure_project_id + "/blueprints/" + cloudendure_blueprint_id
@@ -141,6 +172,10 @@ def update_blueprint(http_client, cloudendure_url, cloudendure_project_id, cloud
 
     if resp.status_code != 200:
         raise Exception('Unable to update blueprint, response code:', resp.status_code, resp.reason)
+        # TODO: switch this to HTTPError
+        # raise Exception('Unable to update blueprint, response code:', resp.status_code, resp.reason)
+    else:
+        print(f'Update blueprint {cloudendure_blueprint_id} - status {resp.status_code} {resp.reason}')
 
 def get_ec2_instance_sgs(ec2_client, ec2_id):
     try:
